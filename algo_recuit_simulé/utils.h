446143df9aa3432c74c08da69d531197b316d4fe
Douraid BEN-HASSEN <douraid.ben-hassen@imerir.com>
2022-06-08 16:28:17 +0200

    Add new file

---
+++ algo_recuit_simulé/utils.h
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <array>
+#include <ctime>
+
+using namespace std;
+
+enum Color {
+	EMPTY = 0,
+	ROUGE = 1,
+	JAUNE = 2,
+	VERT = 3,
+	BLEU = 4,
+	VIOLET = 5
+};
+
+void init();
+
+// +-------+
+// | UTILS |
+// +-------+
+vector<vector<int>> readCol(const char*);
+vector<string> split(const string&, char);
+int countNbViolation(int, Color);
+int countNbViolation(vector<Color>, int, Color);
+int getRandomNumber(int);
+
+// +--------+
+// | GETTER |
+// +--------+
+int getNbCase();
+int getNbViolation();
+vector<Color> getTab();
+
+// +------+
+// | ALGO |
+// +------+
+void algo_violation();
+void algo_violation(vector<Color>);
+void algo_recuit();
+
+
