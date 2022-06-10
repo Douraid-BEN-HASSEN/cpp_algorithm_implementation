e62d40cf1b9d0da6a48066b337f890bd177e5590
Douraid BEN-HASSEN <douraid.ben-hassen@imerir.com>
2022-06-10 08:38:46 +0200

    Add new file

---
+++ algo_gsat_random_walk_descente/main.cpp
@@ -0,0 +1,46 @@
+// +------+
+// | MAIN |
+// +------+
+#include "utils.h"
+
+int equipe = 5;
+string input_path = "Graphes/";
+string input_file_name = "queen5_5.col";
+string output_file_name = "output.txt";
+
+void FileWrite(string output_file);
+
+int main()
+{
+    init();
+
+    algo_descente();
+    algo_gsat();
+    algo_random_walk();
+
+    FileWrite(output_file_name);
+
+}
+
+void FileWrite(string output_file_name)
+{
+    vector<Color> tab = getTab();
+    string algo = "recuit gsat";
+    ofstream output_file(output_file_name, ios::out | ios::trunc);
+    if (output_file)
+    {
+        output_file << "c Algo utilisé : " << algo << endl;
+        output_file << "c Equipe : " << equipe << endl;
+        output_file << "f " << input_file_name << endl;
+        output_file << "r 5 " << getNbViolation() << endl;
+        for (int i = 0; i < 25; i++)
+        {
+            output_file << tab[i] << endl;
+        }
+        output_file.close();
+    }
+    else
+    {
+        cerr << "Impossible to write in the file !" << endl;
+    }
+}
