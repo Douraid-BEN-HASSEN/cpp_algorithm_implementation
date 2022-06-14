b1838343d48eec8721364543fcc657abe73b17df
Romain GRANJON <romain.granjon@imerir.com>
2022-06-14 10:49:47 +0200

    Ajout fichiers supprimés

---
+++ main.cpp
@@ -0,0 +1,61 @@
+// +------+
+// | MAIN |
+// +------+
+#include "utils.h"
+
+int equipe = 5;
+string input_path = "Graphes/";
+string output_file_name = "output.txt";
+
+void FileWrite(string output_file);
+bool checkFile();
+
+int main()
+{
+    init();
+
+    // algo_recuit(); // OK
+    // algo_descente(); // OK
+    // algo_gsat();
+    // algo_random_walk(); OK
+
+    // algo_recherche_tabou(); // OK
+    // algo_gww(); // OK
+    algo_genetique();
+
+    FileWrite(output_file_name);
+
+    // checkFile();
+}
+
+void FileWrite(string output_file_name)
+{
+    vector<Color> tab = getTab();
+    ofstream output_file(output_file_name, ios::out | ios::trunc);
+    if (output_file)
+    {
+        output_file << "c Algo utilisé : " << getAlgo() << endl;
+        output_file << "c Equipe : " << equipe << endl;
+        output_file << "f " << getFileName() << endl;
+        output_file << "r " << getnCouleurDifferente() << " " << getNbViolation() << endl;
+        for (int i = 0; i < getNbCase(); i++)
+        {
+            output_file << tab[i] << endl;
+        }
+        output_file.close();
+    }
+    else
+    {
+        cerr << "Impossible to write in the file !" << endl;
+    }
+}
+
+bool checkFile() {
+    int nbViolationTmp = 0;
+    vector<Color> tab = readOutput("output.txt");
+    for (int iCase = 0; iCase < getNbCase(); iCase++) {
+        nbViolationTmp += countNbViolation(tab, iCase, tab[iCase]);
+    }
+    nbViolationTmp /= 2;
+    return nbViolationTmp == nbViolationTmp;
+}
---
+++ utils.h
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <array>
+#include <ctime>
+#include <math.h>
+
+using namespace std;
+
+enum Color {
+	EMPTY = 0,
+	ROUGE = 1,
+	JAUNE = 2,
+	VERT = 3,
+	BLEU = 4,
+	VIOLET = 5,
+	ORANGE = 6,
+	NOIR = 7,
+	BLANC = 8,
+	ROSE = 9
+};
+
+void init();
+
+// +-------+
+// | UTILS |
+// +-------+
+vector<vector<int>> readCol(const char*);
+vector<string> split(const string&, char);
+int countNbViolation(int, Color);
+int countNbViolation(vector<Color>, int, Color);
+int countNbViolation(vector<Color>);
+int getRandomNumber(int);
+vector<Color> readOutput(const char*);
+bool compareGrille(vector<Color>, vector<Color>);
+
+// +--------+
+// | GETTER |
+// +--------+
+int getNbCase();
+int getNbViolation();
+string getAlgo();
+vector<Color> getTab();
+int getnCouleurDifferente();
+string getFileName();
+
+// +------+
+// | ALGO |
+// +------+
+void algo_violation();
+void algo_violation(vector<Color>);
+
+void algo_recuit();
+
+void algo_descente();
+void algo_gsat();
+void algo_random_walk();
+
+void algo_recherche_tabou();
+
+vector<Color> algo_gww_random_walk(vector<Color>);
+void algo_gww();
+
+vector<vector<Color>> algo_genetique_generate_population(int);
+vector<vector<Color>> algo_genetique_selection_uniforme(vector<vector<Color>>);
+vector<vector<Color>> algo_genetique_selection_rank(vector<vector<Color>>);
+vector<vector<Color>> algo_genetique_croissement(vector<vector<Color>>, int, int);
+vector<Color> algo_genetique_get_best(vector<vector<Color>>);
+vector<vector<Color>> algo_genetique_mutation(vector<vector<Color>>);
+
+void algo_genetique();
