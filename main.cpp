c0afb5568943b1d402dc64096255b47727a205cf
Douraid BEN-HASSEN <douraid.ben-hassen@imerir.com>
2022-06-17 09:39:27 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,37 +1,51 @@
 // +------+
 // | MAIN |
 // +------+
+#include <Windows.h>
+
 #include "utils.h"
 
-bool checkFile();
-
-int main()
+int main(int argc, char* argv[])
 {
+
+    if (argc > 4) {
+
+        std::cout << "[0] " << argv[1] << std::endl;
+        std::cout << "[1] " << argv[2] << std::endl;
+        std::cout << "[2] " << argv[3] << std::endl;
+        std::cout << "[3] " << argv[4] << std::endl;
+
+        SetConsoleTitleA(argv[4]);
+
+        setreadFromOutput(argv[1]);
+        setnCouleurDifferente(atoi(argv[2]));
+        setFolder(argv[3]);
+        setFilename(argv[4]);
+
+        
+    }
+    else if(argc == 4) {
+
+        SetConsoleTitleA(argv[4]);
+
+        std::cout << "[0] " << argv[1] << std::endl;
+        std::cout << "[1] " << argv[2] << std::endl;
+        std::cout << "[2] " << argv[3] << std::endl;
+        std::cout << "[3] " << argv[4] << std::endl;
+
+        setreadFromOutput(argv[1]);
+        setnCouleurDifferente(atoi(argv[2]));
+        setFolder(argv[3]);
+        setFilename(argv[4]);
+    }
+    else {
+        setreadFromOutput("D:/META_P2_final/x64/Debug/result/output_latin_square_10.txt");
+        setnCouleurDifferente(97);
+        setFolder("D:/META_P2_final/META_P2/");
+        setFilename("latin_square_10.col");
+    }
+
     init();
 
-    // algo_recuit(); // OK
-    // algo_descente(); // OK
-    // algo_gsat();
-    // algo_random_walk(); OK
-
-    // algo_recherche_tabou(); // OK
-    // algo_gww(); // OK
-    // algo_genetique();
-
     algo_hybride();
-    //algo_hybride_partie_2();
-
-    
-
-    // checkFile();
-}
-
-bool checkFile() {
-    int nbViolationTmp = 0;
-    vector<Color> tab = readOutput("output.txt");
-    for (int iCase = 0; iCase < getNbCase(); iCase++) {
-        nbViolationTmp += countNbViolation(tab, iCase, tab[iCase]);
-    }
-    nbViolationTmp /= 2;
-    return nbViolationTmp == nbViolationTmp;
 }
